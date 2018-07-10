420ea9197c3ca3c21cabd581b1d07e294fc4d259
Nolan Kingdon <nolan.kingdon@powerschool.com>
2018-07-10 15:54:20 -0400

    Removed firebase

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import * as firebase from 'firebase';
-
-const config = {
-
-  };
-
-  export const fbCon = firebase.initializeApp(config);
-  export const fbConTodos = firebase.database().ref('todos');
-  export const fbConClients = firebase.database().ref('clients');
