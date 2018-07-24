64defdc36b3640cfe1ea4236a08cd10a3cf1ad53
nolanKingdon <nolan.kingdon@gmail.com>
2018-07-24 15:49:21 -0400

    Added router functionality to play nicely with the new account features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import TaskMaster from './TodoList/TaskMaster';
 import ClientMaster from './ClientTracker/clientMaster';
 import TimelineGeneratorMaster from './timeline-generator/TimelineGeneratorMaster';
+import Login from './login/login';
+import Logout from './logout/logout'
 import './css/app-styles.css'
 
 class App extends Component {
@@ -10,7 +12,11 @@ class App extends Component {
     return(
         <div>
           <h1 style = {{textAlign: "center"}}>Efficiency Tool</h1>
+          <div className = "logout-widget">
+            <Logout />
+          </div>
           <div className = "grid-wrapper">
+
             <ClientMaster />
             <TaskMaster />
             <TimelineGeneratorMaster />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
 import App from './components/App.jsx';
+import Login from './components/login/login.jsx';
 
 ReactDOM.render(
-  <App />, document.getElementById("root")
+  <Router path = "/" history = { browserHistory }>
+    <Route path = "/login" component = { Login } />
+    <Route path = "/app" component = { App } />
+  </Router>, document.getElementById("root")
 )
