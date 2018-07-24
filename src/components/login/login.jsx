6f9266f9ba856098380807a8a3a11dc420e2ee80
nolanKingdon <nolan.kingdon@gmail.com>
2018-07-24 15:48:58 -0400

    Added user account functionality w/ login/logout

---
+++ src/components/login/login.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import "./css/styles.css";
+import { browserHistory } from 'react-router';
+import * as firebase from 'firebase';
+
+class Login extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        browserHistory.push("/app");
+      } else {
+
+      }
+    })
+  }
+
+  submit(){
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error){
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    })
+  }
+
+  render(){
+    return(
+      <div className = "login">
+        <h1>Efficiency Tool login</h1>
+        <input
+          type = "text"
+          className = "login-input"
+          id = "login-userName"
+          placeholder = "Username"
+          onChange = { event => this.setState({ email: event.target.value})}
+        />
+        <input
+          type = "password"
+          className = "login-input"
+          id = "login-pass"
+          placeholder = "Password"
+          onChange = { event => this.setState({password: event.target.value})}
+        />
+      <button
+        type="button"
+        className = "btn btn-success"
+        onClick = {() => this.submit()}
+      >
+        login
+      </button>
+      </div>
+
+    )
+  }
+
+}
+
+export default Login;
---
+++ src/components/logout/logout.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import { browserHistory } from 'react-router';
+
+class Logout extends Component {
+
+  logout(){
+    firebase.auth().signOut().then(() => {
+      browserHistory.push("/login");
+    }).catch(function(error){
+      alert(error)
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <button
+          className = "btn btn-danger"
+          onClick = {() => this.logout()}
+        >
+        Logout
+      </button>
+      </div>
+
+    )
+  }
+}
+
+export default Logout;
